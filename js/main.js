50a7213c7f3a0b3f2ebb2e8468c1188937274854
Niariasve <nestorariasve@gmail.com>
2024-06-09 23:30:20 -0500

    guia 8

--- js/main.js
+++ js/main.js
@@ -8,32 +8,108 @@ let loaded = (e) => {
         let email = document.getElementById('email')
         let contraseña = document.getElementById('contraseña')
         let confirmar_contraseña = document.getElementById('contraseña-confirmar')
+        let genero = document.getElementById('genero')
+
+        let errores = []
         
         if (name.value.length === 0) {
             name.focus()
-            alert('Ingrese un texto válido')
+            errores.push('Es obligatorio ingresar un nombre')
         }
         
         if(apellido.value.length === 0) {
             apellido.focus()
-            alert('Ingrese un texto válido')
+            errores.push('Es obligatorio ingresar el apellido')
         }
 
         if(email.value.length === 0) {
             email.focus()
-            alert('Ingrese un texto válido')
+            errores.push('Es obligatorio ingresar email')
         }
 
         if(contraseña.value.length === 0) {
             contraseña.focus()
-            alert('Ingrese un texto válido')
+            errores.push('Es obligatorio ingresar una contraseña')
         }
 
         if(confirmar_contraseña.value.length === 0) {
             confirmar_contraseña.focus()
-            alert('Ingrese un texto válido')
+            errores.push('Es obligatorio ingresar confirmar su contraseña')
+        }
+
+        if(genero.value === 0) {
+            errores.push('Es obligatorio ingresar un género')
+        }
+
+        if(errores.length > 0) {
+            mostrarErrores(errores)
+        } else {
+            guardarObjeto(name.value, apellido.value, email.value, contraseña.value, confirmar_contraseña.value, genero.value)
         }
     })
 }
+
+function guardarObjeto(name, apellido, email, contraseña, confirmar_contraseña, genero) {
+    const datos = {
+        nombre: name,
+        apellido: apellido,
+        email: email,
+        contraseña: contraseña,
+        confirmar_contraseña: confirmar_contraseña,
+        genero: genero
+    }
+
+    console.log(datos)
+
+    fetch('https://landing-page-36bed-default-rtdb.firebaseio.com/collection.json', {
+        method: 'POST',
+        body: JSON.stringify(datos),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(respuesta => {
+        if (!respuesta.ok) {
+            throw new Error('Error en la respuesta del servidor');
+        }
+        return respuesta.json();
+    })
+    .then(datos => {
+        console.log(datos);
+        let alertas = document.querySelector('.errores');
+        alertas.innerHTML = `
+            <div class="error">
+                <p>Exito</p>
+            </div>
+        `;
+    })
+    .catch(error => {
+        console.error(error);
+        let alertas = document.querySelector('.errores');
+        alertas.innerHTML = `
+            <div class="error">
+                <p>Error: ${error.message}</p>
+            </div>
+        `;
+    });
+}
+
+
+function mostrarErrores(errores) {
+    let documentErrores = document.querySelector('.errores')
+
+    documentErrores.innerHTML = ""
+
+    errores.forEach(element => {
+        let html = `
+            <div class="error">
+                <p>${element}</p>
+            </div>
+        `
+
+        documentErrores.innerHTML += html
+    });
+}
+
 
 window.addEventListener("DOMContentLoaded", loaded)
